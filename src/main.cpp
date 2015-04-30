08511e6cfb7b015f7ace154d5ab8caaba1592c50
Srinivas Kaza <kazasrinivas3@gmail.com>
2015-04-30 16:50:30 -0400

    Completes support for command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -52,56 +52,60 @@ int main(int argc, char* argv[]) {
 	}
 	if (std::find(argv, argv + argc, NUM_CITY_DELIMITER) != argv + argc)
 	{
-		int numCities = std::stoi(*(std::find(argv, argv + argc, NUM_CITY_DELIMITER) + 1));
-		tour = new Tour(numCities);
+		try {
+			int numCities = std::stoi(*(std::find(argv, argv + argc, NUM_CITY_DELIMITER) + 1));
+			tour = new Tour(numCities);
+		} catch (...) {
+			std::cerr << "City number not given. " + correctUsage << std::endl;
+			return 1;
+		}
+
 	}
 	else
 	{
 		std::cout << "Defaulting to 100 cities" << std::endl;
 		tour = new Tour(DEFAULT_CITY_NUM);
 	}
+
+	path * solution;
 	if (std::find(argv, argv + argc, ALGORITHM_DELIMITER) != argv + argc)
 	{
-		std::string algorithm = std::string(*(std::find(argv, argv + argc, ALGORITHM_DELIMITER) + 1));
+		std::string algorithm;
+		try {
+			algorithm = std::string(*(std::find(argv, argv + argc, ALGORITHM_DELIMITER) + 1));
+		} catch (...) {
+			std::cerr << "Algorithm not given. " + correctUsage << std::endl;
+			return 1;
+		}
 
 		if (algorithm == ALGORITHM_RANDOM)
 		{
 			std::cout << "Using the random algorithm" << std::endl;
+			solution = new path(tour->solveRandom());
 		} else if (algorithm == ALGORITHM_TWO_OPT)
 		{
 			std::cout << "Using the 2-opt algorithm" << std::endl;
+			solution = new path(tour->solveRandomWithSwitches(3500.0, 500));
 		} else if (algorithm == ALGORITHM_SIMULATED_ANNEALING)
 		{
 			std::cout << "Using simulated annealing" << std::endl;
+			solution = new path(tour->solveSimulatedAnnealing(20000.0, 20, 0.1));
 		}
 		else
 		{
-			std::cerr << "Invalid algorithm" << std::endl;
+			std::cerr << "Invalid algorithm. " + correctUsage << std::endl;
 			return 1;
 		}
 	}
+	else
+		solution = new path(tour->solveSimulatedAnnealing(20000.0, 20, 0.1));
 
 	tour->printGraph();
 	std::cout << std::endl;
-	std::vector< Graph::Edge > randomSolution = tour->solveRandom();
-	std::vector< Graph::Edge > twoOptSolution = tour->solveRandomWithSwitches(3500.0, 500);
-	std::vector< Graph::Edge > simulatedAnnealingSolution = tour->solveSimulatedAnnealing(20000.0, 20, 0.1);
 
-	std::cout << std::endl << "Random Solution: " << std::endl;
+	Tour::printPath(*solution);
 
-	Tour::printPath(randomSolution);
-
-	std::cout << std::endl << "2-Opt Solution: " << std::endl;
-
-	Tour::printPath(twoOptSolution);
-
-	std::cout << std::endl << "Simulated Annealing Solution: " << std::endl;
-
-	Tour::printPath(simulatedAnnealingSolution);
-
-	std::cout << std::endl << "Random Solution: " << tour->getTourLength(randomSolution) << std::endl;
-	std::cout << "2-Opt Tour length: " << tour->getTourLength(twoOptSolution) << std::endl;
-	std::cout << "Simulated Annealing Solution: " << tour->getTourLength(simulatedAnnealingSolution) << std::endl;
+	std::cout << std::endl << "Tour Length: " << tour->getTourLength(*solution) << std::endl;
 
 	if (!Tour::disableGUI)
 		tour->getDisplayThread()->join();
